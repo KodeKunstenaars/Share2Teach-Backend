f5bf7ae31ef62641c05d1ef5333dbca94973810e
Gerhard Otto <gerrieotto123@gmail.com>
2024-08-09 12:27:47 +0200

    add folder structure

--- cmd/Share2Teach/main.go
+++ cmd/Share2Teach/main.go
@@ -2,17 +2,17 @@ package main
 
 import (
 	"context"
-	"github.com/KodeKunstenaars/Share2Teach/db"
+	"github.com/KodeKunstenaars/Share2Teach/internal/database"
 	"log"
 )
 
 func main() {
 	// Connect to the database
-	db.Connect()
+	connect.Connect()
 
 	// Ensure disconnection from the database when the main function ends
 	defer func() {
-		if err := db.Client.Disconnect(context.TODO()); err != nil {
+		if err := connect.Client.Disconnect(context.TODO()); err != nil {
 			log.Fatalf("Failed to disconnect from MongoDB: %v", err)
 		}
 	}()
